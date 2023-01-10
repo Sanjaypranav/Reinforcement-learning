8c62c755a14601a5d94ec291ca2f50dd3c85c36a
Sanjaypranav V M <sanjaypriya195@gmail.com>
2023-01-10 09:35:08 +0000

    initial commit

---
+++ ex2.1.py
@@ -0,0 +1,8 @@
+import gym 
+import numpy as np 
+
+# create FrozenLake environment v1 
+
+# apply optimal policy on the environment
+
+env = gym.make('FrozenLake-v1', is_slippery=False, map_name="4x4")
